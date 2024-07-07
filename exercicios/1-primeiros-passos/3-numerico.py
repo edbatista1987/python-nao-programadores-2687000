5071fe64cf78daabd6236a299864d0ed9a306704
Edbatista1987 <77135376+edbatista1987@users.noreply.github.com>
2024-07-07 21:49:37 +0000

    corrigido

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -2,10 +2,21 @@ data_nascimento = '05-10-1976'
 idade_numerica = 46
 altura = 1.74
 
-# Descubra o tipo de dado de cada variável acima
+data_nascimento = '05-10-1976'
+idade_numerica = 46
+altura = 1.74
 
+# Descubra o tipo de dado de cada variável
+print(type(data_nascimento))  # <class 'str'>
+print(type(idade_numerica))   # <class 'int'>
+print(type(altura))           # <class 'float'>
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+# Vamos repetir a string 'idade_numerica' vezes
+print(data_nascimento * idade_numerica)  # Resultado: '05-10-197605-10-1976... (46 vezes)'
 
 # Realize uma operação entre dados do tipo inteiro e float
+# Vamos multiplicar idade_numerica por altura
+resultado = idade_numerica * altura
+print(resultado)  # Resultado: 80.04
+
--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -1,10 +1,17 @@
 ano_nascimento = 1989
 ano_formatura = 2010
 
-# Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
 
+# 1. Descubra a idade de Gerlaine no ano da sua formatura
+idade_na_formatura = ano_formatura - ano_nascimento
+print(idade_na_formatura)  # Resultado: 21
 
-# Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
+# 2. Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
+print(ano_formatura > ano_nascimento)    # True, pois 2010 > 1989
+print(ano_nascimento <= ano_formatura)   # True, pois 1989 <= 2010
+print(ano_nascimento == ano_formatura)   # False, pois 1989 não é igual a 2010
 
-
-# Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+# 3. Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+print((ano_nascimento < ano_formatura) and (idade_na_formatura == 21))  # True, ambas as condições são verdadeiras
+print((ano_nascimento > ano_formatura) or (idade_na_formatura == 21))   # True, uma das condições é verdadeira
+print(not (ano_nascimento == ano_formatura))                            # True, nega a condição falsa, resultando em verdadeiro
